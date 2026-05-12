23d5acc5a4425d9dc7bb75c1ed146a21cdb4b583
AurelienDillies <aureliendillies2001@gmail.com>
2026-05-12 16:02:29 +0200

    feat : ajout de la page inscription

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Inscription } from './pages/inscription/inscription';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'inscription', component: Inscription }
+];
