9bbf0375e0f155ae6f332572b20630c4db2a7f60
AurelienDillies <aureliendillies2001@gmail.com>
2026-05-21 14:44:14 +0200

    style: ajout du l'interaction utilisateur, sur header / footer / connexion / inscription / profil

---
+++ frontend/src/app/components/forms-profile/forms-profile.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms-profile',
+  imports: [],
+  templateUrl: './forms-profile.html',
+  styleUrl: './forms-profile.css',
+})
+export class FormsProfile {}
--- frontend/src/app/pages/profile/profile.ts
+++ frontend/src/app/pages/profile/profile.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsProfile } from '../../components/forms-profile/forms-profile';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [FormsProfile, RouterLink],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
