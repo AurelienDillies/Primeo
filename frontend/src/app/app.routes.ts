f21ab87890c6c67fad3a82bff7f898dc538db547
AurelienDillies <aureliendillies2001@gmail.com>
2026-05-21 10:42:06 +0200

    feat: ajout page not found

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,6 +7,9 @@ import { Courses } from './pages/courses/courses';
 import { Activities } from './pages/activities/activities';
 import { Progress } from './pages/progress/progress';
 import { Messages } from './pages/messages/messages';
+import { LegalNotice } from './pages/legal-notice/legal-notice';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'connexion', pathMatch: 'full' },
@@ -17,5 +20,8 @@ export const routes: Routes = [
     { path: 'cours', component: Courses},
     { path: 'activites', component: Activities },
     { path: 'suivi', component: Progress },
-    { path: 'messagerie', component: Messages }
+    { path: 'messagerie', component: Messages },
+    { path: 'mentions-legales', component: LegalNotice },
+    { path: 'politique-confidentialite', component: PrivacyPolicy },
+    { path: '**', component: NotFound}
 ];
