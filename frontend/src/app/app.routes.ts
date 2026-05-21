c810aa6aa1a2d87f8544443fe8973d5446b1f792
AurelienDillies <aureliendillies2001@gmail.com>
2026-05-21 10:35:25 +0200

    feat: ajout des pages de navigation et de leur redirection

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,21 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register} from './pages/register/register';
+import { Profile } from './pages/profile/profile';
+import { Classes } from './pages/classes/classes';
+import { Courses } from './pages/courses/courses';
+import { Activities } from './pages/activities/activities';
+import { Progress } from './pages/progress/progress';
+import { Messages } from './pages/messages/messages';
 
 export const routes: Routes = [
-    { path: "", component: Login },
-    { path: 'inscription', component: Register }
+    { path: '', redirectTo: 'connexion', pathMatch: 'full' },
+    { path: 'connexion', component: Login },
+    { path: 'inscription', component: Register },
+    { path: 'profil', component: Profile},
+    { path: 'classes', component: Classes },
+    { path: 'cours', component: Courses},
+    { path: 'activites', component: Activities },
+    { path: 'suivi', component: Progress },
+    { path: 'messagerie', component: Messages }
 ];
---
+++ frontend/src/app/pages/activity-details/activity-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-details',
+  imports: [],
+  templateUrl: './activity-details.html',
+  styleUrl: './activity-details.css',
+})
+export class ActivityDetails {}
