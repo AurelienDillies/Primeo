c5cc8c790cea8e25bcf29eeab04d5b2041e13bd7
AurelienDillies <aureliendillies2001@gmail.com>
2026-05-26 14:50:37 +0200

    styles : ajout du style de la page classe

---
+++ frontend/src/app/components/classe/classe.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-classe',
+  imports: [RouterLink],
+  templateUrl: './classe.html',
+  styleUrl: './classe.css',
+})
+export class Classe {}
