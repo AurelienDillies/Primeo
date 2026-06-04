f2543006fdd345aa3f64ebe69d11a770813c7928
AurelienDillies <aureliendillies2001@gmail.com>
2026-06-04 10:46:45 +0200

    feat: ajout des routes et des pages des contenu des activité et cours

--- frontend/src/app/pages/course-details/course-details.ts
+++ frontend/src/app/pages/course-details/course-details.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-course-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './course-details.html',
   styleUrl: './course-details.css',
 })
