158605c17923b46f08effbf6731872c403e79659
AurelienDillies <aureliendillies2001@gmail.com>
2026-06-05 14:07:16 +0200

    style: ajout style et template de la page suivi et not-found

--- frontend/src/app/components/advance/advance.ts
+++ frontend/src/app/components/advance/advance.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-advance',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './advance.html',
   styleUrl: './advance.css',
 })
