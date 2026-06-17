eb53c9f4e9976984b6a6a0c4c893b36763017db6
AurelienDillies <aureliendillies2001@gmail.com>
2026-06-17 12:10:23 +0200

    style: ajout du style pour le bonton créer classes/cours/acitive

--- frontend/src/app/pages/classes/classes.ts
+++ frontend/src/app/pages/classes/classes.ts
@@ -6,6 +6,7 @@ import { UserService } from '../../services/user-service';
 import { CommonModule } from '@angular/common';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { RouterLink } from '@angular/router';
 
 type ClassesVM = {
   classes: any[];
@@ -16,34 +17,39 @@ type ClassesVM = {
 @Component({
   selector: 'app-classes',
   standalone: true,
-  imports: [CommonModule, Classe],
+  imports: [CommonModule, Classe, RouterLink],
   templateUrl: './classes.html',
   styleUrl: './classes.css',
 })
 export class Classes {
-
   vm$: Observable<ClassesVM>;
 
   constructor(private userService: UserService) {
-
     const userId = this.userService.getUserId();
 
     this.vm$ = userId
       ? this.userService.getUserInfo(userId).pipe(
-          map((userInfo: any) => ({
-            classes: userInfo.classes ?? [],
-            student: userInfo.roles.includes('ROLE_STUDENT')
-              ? userInfo as Student
-              : null,
-            teacher: userInfo.roles.includes('ROLE_TEACHER')
-              ? userInfo as User
-              : null
-          }))
-        )
+        map((userInfo: any) => ({
+          classes: userInfo.classes ?? [],
+          student: userInfo.roles.includes('ROLE_STUDENT')
+            ? (userInfo as Student)
+            : null,
+          teacher: userInfo.roles.includes('ROLE_TEACHER')
+            ? (userInfo as User)
+            : null,
+        }))
+      )
       : of({
-          classes: [],
-          student: null,
-          teacher: null
-        });
+        classes: [],
+        student: null,
+        teacher: null,
+      });
+  }
+
+  get canCreateClass(): boolean {
+    return this.userService.hasAnyRole([
+      'ROLE_ADMIN',
+      'ROLE_TEACHER',
+    ]);
   }
 }
--- frontend/src/app/pages/courses/courses.ts
+++ frontend/src/app/pages/courses/courses.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { Course } from '../../components/course/course';
+import { UserService } from '../../services/user-service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
-  imports: [Course],
+  imports: [Course, RouterLink],
   templateUrl: './courses.html',
   styleUrl: './courses.css',
 })
-export class Courses {}
+export class Courses {
+  readonly rolesForChange = ['ROLE_ADMIN', 'ROLE_TEACHER'];
+
+  constructor(public userService: UserService) { }
+}
